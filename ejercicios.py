51bcf5aed868f4016efb8f409f3d6550856c60be
Victoriino <114113512+Victoriino@users.noreply.github.com>
2023-04-16 16:05:28 -0500

    6

---
+++ ejercicios.py
@@ -0,0 +1,40 @@
+
+'''intentos = 3
+while intentos > 0:
+	if input('>>Escriba la contraseña') == 'bitboss':
+		print('Correcta')
+		break
+	intentos = intentos - 1 
+	print('contraseña incorrecta :c')
+else:
+	print('Se te acabron los intentos')'''
+ 
+ 
+"""intentos = 1
+while intentos != 0:
+	if input('¿Desea continuar el programa?') != 'sí':
+		print('Hasta la vista bro')
+		break
+	intentos = intentos + 1 """
+
+"""pregunta = input('¿Desea terminar el programa? ')
+while pregunta != "S" and pregunta != "s":
+    pregunta = input('¿Desea terminar el programa? ')
+else:
+    print('Hasta la vista')"""
+    
+    
+tallas = input('Escriba la palabra de la que quiera que se haga la plantilla de un acróstico')
+while tallas == "Amor":
+    for iterar in tallas:
+        print(iterar)
+        break
+else:
+    print('Se escribió un valor sin sentido ')
+[]
+
+"""tallas = 'letras'
+for iterar in tallas:
+    print(iterar)"""
+
+    
